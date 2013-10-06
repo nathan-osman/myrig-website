6e9dae4f1b653abed5a8cf069274bf111623b71d
Nathan Osman <admin@quickmediasolutions.com>
2013-10-05 17:42:17 -0700

    Added serving of media files to urls.py.

--- myrig/urls.py
+++ myrig/urls.py
@@ -1,17 +1,17 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myrig.views.home', name='home'),
-    # url(r'^myrig/', include('myrig.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    
+    # Administration interface
+    url(r'^admin/', include(admin.site.urls)),
+    
 )
+
+# When running in DEBUG mode, serve media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
